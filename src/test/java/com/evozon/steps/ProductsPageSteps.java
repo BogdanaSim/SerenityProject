893edbb5abd4e6cb717654aa20ed8f952f4d4b96
AndreeaStejerean19 <andreeastejerean19@gmail.com>
2022-08-08 17:16:46 +0300

    addded click on first product

---
+++ src/test/java/com/evozon/steps/ProductsPageSteps.java
@@ -0,0 +1,12 @@
+package com.evozon.steps;
+
+import com.evozon.pages.ProductsPage;
+import net.thucydides.core.annotations.Step;
+
+public class ProductsPageSteps {
+
+    private ProductsPage productPage;
+
+    @Step
+    public void clickOnFirstProduct() { productPage.clickOnFirstProduct(); }
+}
