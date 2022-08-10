8dfbba33cc9cc2e4dac2beb8e98df5f33362d02a
AndreeaStejerean19 <andreeastejerean19@gmail.com>
2022-08-10 14:41:30 +0300

    addded search test for individual steps

--- src/test/java/com/evozon/features/SearchTests.java
+++ src/test/java/com/evozon/features/SearchTests.java
@@ -4,6 +4,7 @@ import com.evozon.steps.HomepageSteps;
 import com.evozon.steps.LoginSteps;
 import com.evozon.steps.SearchSteps;
 import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
+import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.junit.annotations.UseTestDataFrom;
@@ -11,8 +12,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
-@RunWith(SerenityParameterizedRunner.class)
-    @UseTestDataFrom(value="src/test/resources/csv/search.csv")
+import java.io.IOException;
+
+import static net.thucydides.core.steps.stepdata.StepData.withTestDataFrom;
+
+@RunWith(SerenityRunner.class)
+   // @UseTestDataFrom(value="src/test/resources/csv/search.csv")
 
     public class SearchTests {
         @Managed(uniqueSession = true )
@@ -24,14 +29,14 @@ import org.openqa.selenium.WebDriver;
         @Steps
         private SearchSteps searchSteps;
 
-        private String sku,nameProduct;
+       // private String sku,nameProduct;
 
         @Test
-    public void searchTest() {
+    public void searchTest() throws IOException {
             homepageSteps.navigateToHomepage();
-            searchSteps.searchText(sku);
-            searchSteps.clickSearchButton();
-            searchSteps.checkSearchedProduct(nameProduct);
-
+            withTestDataFrom("src/test/resources/csv/search.csv").run(searchSteps).searchSteps();
+//            searchSteps.searchText();
+//            searchSteps.clickSearchButton();
+//            searchSteps.checkSearchedProduct();
         }
     }
--- src/test/java/com/evozon/steps/SearchSteps.java
+++ src/test/java/com/evozon/steps/SearchSteps.java
@@ -3,23 +3,32 @@ package com.evozon.steps;
 import com.evozon.pages.ProductsPage;
 import com.evozon.pages.SearchPage;
 import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
 import org.junit.Assert;
 
-public class SearchSteps {
+public class SearchSteps extends ScenarioSteps {
 
     private SearchPage searchPage;
-
     private ProductsPage productsPage;
 
+    private String sku, nameProduct;
+
     @Step
-    public void searchText(String searchText) {searchPage.setSearchField(searchText);}
+    public void searchText() {searchPage.setSearchField(sku);}
 
     @Step
     public void clickSearchButton(){searchPage.clickOnSearchButton();}
 
     @Step
-    public void checkSearchedProduct(String productName){
-        Assert.assertTrue(productName.equalsIgnoreCase(productsPage.getNameOfFirstProductOnPage()));
+    public void checkSearchedProduct(){
+        Assert.assertTrue(nameProduct.equalsIgnoreCase(productsPage.getNameOfFirstProductOnPage()));
+    }
+
+    @Step
+    public void searchSteps() {
+        searchText();
+        clickSearchButton();
+        checkSearchedProduct();
     }
 
 }
